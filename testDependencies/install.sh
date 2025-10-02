37cba9b6082a86c94ac7582b3780a77774c5cac0
Alessandro Autiero <alautiero@gmail.com>
2025-10-02 16:46:43 +0200

    Initial commit

---
+++ testDependencies/install.sh
@@ -0,0 +1,6 @@
+mvn install:install-file \
+  -Dfile=./signal-protocol-java.jar \
+  -DgroupId=archived.org.whispersystems \
+  -DartifactId=signal-protocol-java \
+  -Dversion=2.8.1 \
+  -Dpackaging=jar
